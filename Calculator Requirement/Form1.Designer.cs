3235e12f5ebd5f592f4df84a04346ff1fbd5acd3
brin207 <brenbuan207@gmail.com>
2021-05-01 20:28:35 +0800

    05/01/21 - 8:28PM
    BG Color

--- Calculator Requirement/Form1.Designer.cs
+++ Calculator Requirement/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Calculator_Requirement
 {
-    partial class Form1
+    partial class calcDesign
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,17 +29,20 @@ namespace Calculator_Requirement
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(calcDesign));
             this.SuspendLayout();
             // 
-            // Form1
+            // calcDesign
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ForeColor = System.Drawing.SystemColors.Window;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "calcDesign";
+            this.Opacity = 0.9D;
+            this.Text = "Calculator";
             this.ResumeLayout(false);
 
         }
