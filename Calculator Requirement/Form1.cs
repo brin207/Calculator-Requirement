6ad73ca57432bf03a66b9ac5f4cf16992862c4c0
brin207 <brenbuan207@gmail.com>
2021-05-24 20:19:23 +0800

    05/23/21 - 8:18PM
    +:
    -Added if statements in button clicks

--- Calculator Requirement/Form1.cs
+++ Calculator Requirement/Form1.cs
@@ -21,12 +21,22 @@ namespace Calculator_Requirement
 
         private void button6_Click(object sender, EventArgs e)
         {
-            outptPreview.Text = outptPreview.Text + "2";
+
+            if ((outptPreview.Text == "0") || (outptPreview.Text == ""))
+            {
+
+                outptPreview.Text = outptPreview.Text + "2";
+            }
         }
 
         private void button10_Click(object sender, EventArgs e)
         {
-            outptPreview.Text = outptPreview.Text + "1";
+
+            if ((outptPreview.Text == "0") || (outptPreview.Text == ""))
+            {
+
+                outptPreview.Text = outptPreview.Text + "1";
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -46,47 +56,91 @@ namespace Calculator_Requirement
 
         private void btn1_Click(object sender, EventArgs e)
         {
-            outptPreview.Text = outptPreview.Text + "1";
+
+            if ((outptPreview.Text == "0") || (outptPreview.Text == "")){
+
+                outptPreview.Text = outptPreview.Text + "1";
+            }
         }
 
         private void btn3_Click(object sender, EventArgs e)
         {
-            outptPreview.Text = outptPreview.Text + "3";
+
+            if ((outptPreview.Text == "0") || (outptPreview.Text == ""))
+            {
+
+                outptPreview.Text = outptPreview.Text + "3";
+            }
         }
 
         private void btn4_Click(object sender, EventArgs e)
         {
-            outptPreview.Text = outptPreview.Text + "4";
+
+            if ((outptPreview.Text == "0") || (outptPreview.Text == ""))
+            {
+
+                outptPreview.Text = outptPreview.Text + "4";
+            }
         }
 
         private void btn5_Click(object sender, EventArgs e)
         {
-            outptPreview.Text = outptPreview.Text + "5";
+
+            if ((outptPreview.Text == "0") || (outptPreview.Text == ""))
+            {
+
+                outptPreview.Text = outptPreview.Text + "5";
+            }
         }
 
         private void btn6_Click(object sender, EventArgs e)
         {
-            outptPreview.Text = outptPreview.Text + "6";
+
+            if ((outptPreview.Text == "0") || (outptPreview.Text == ""))
+            {
+
+                outptPreview.Text = outptPreview.Text + "6";
+            }
         }
 
         private void btn7_Click(object sender, EventArgs e)
         {
-            outptPreview.Text = outptPreview.Text + "7";
+
+            if ((outptPreview.Text == "0") || (outptPreview.Text == ""))
+            {
+
+                outptPreview.Text = outptPreview.Text + "7";
+            }
         }
 
         private void btn8_Click(object sender, EventArgs e)
         {
-            outptPreview.Text = outptPreview.Text + "8";
+
+            if ((outptPreview.Text == "0") || (outptPreview.Text == ""))
+            {
+
+                outptPreview.Text = outptPreview.Text + "8";
+            }
         }
 
         private void btn9_Click(object sender, EventArgs e)
         {
-            outptPreview.Text = outptPreview.Text + "9";
+
+            if ((outptPreview.Text == "0") || (outptPreview.Text == ""))
+            {
+
+                outptPreview.Text = outptPreview.Text + "9";
+            }
         }
 
         private void btn0_Click(object sender, EventArgs e)
         {
-            outptPreview.Text = outptPreview.Text + "0";
+
+            if ((outptPreview.Text == "0") || (outptPreview.Text == ""))
+            {
+
+                outptPreview.Text = outptPreview.Text + "0";
+            }
         }
 
         private void btnDot_Click(object sender, EventArgs e)
